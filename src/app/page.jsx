35a785117b133007531ab38173b42c519da6dc16
Neri J. Jakubowski Jr <neri@nerijunior.com>
2024-08-30 15:27:38 -0300

    fix build

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -107,7 +107,7 @@ export default function Home() {
           </thead>
           <tbody>
             {Object.keys(totals).map((assunto) => (
-              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+              <tr key={assunto} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
