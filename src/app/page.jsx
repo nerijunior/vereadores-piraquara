4249e12c13d48107e8154cbeaee66fec789dca88
Neri J. Jakubowski Jr <neri@nerijunior.com>
2024-09-16 11:32:12 -0300

    sorte tabler

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,9 +17,22 @@ export default function Home() {
     return acc;
   }, {});
 
+  const sorted = Object.entries(totals).sort((a, b) => {
+    const aValue = a[1];
+    const bValue = b[1];
+
+    if (aValue > bValue) {
+      return -1;
+    } else if (aValue < bValue) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <div className="p-8">
-      <div className="grid grid-cols-4 gap-4 justify-between mb-4">
+      <div className="grid grid-cols-2 gap-2 lg:grid-cols-4 lg:gap-4 justify-between mb-4 overflow-hidden">
         {data.map((row, i) => (
           <div
             key={i}
@@ -91,7 +104,7 @@ export default function Home() {
         ))}
       </div>
 
-      <p class="font-bold 2xl mb-4">Totais</p>
+      <p class="font-bold 2xl mb-4">Assuntos mais pautados</p>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -106,17 +119,18 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {Object.keys(totals).map((assunto) => (
-              <tr key={assunto} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            {sorted.map((assunto) => (
+              <tr
+                key={assunto[0]}
+                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+              >
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  {assunto.split("_").join(" ")}
+                  {assunto[0].split("_").join(" ")}
                 </th>
-                <td className="px-6 py-4">
-                  {totals[assunto]}
-                </td>
+                <td className="px-6 py-4">{assunto[1]}</td>
               </tr>
             ))}
           </tbody>
